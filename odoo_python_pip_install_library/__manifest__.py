41e9ccf89abecf9f414589de4e7c269f78e19b9b
José Gregorio Molina <molinajoseg@gmail.com>
2024-11-17 21:47:23 -0400

    subir modulos

---
+++ odoo_python_pip_install_library/__manifest__.py
@@ -0,0 +1,28 @@
+########################################################################
+#                                                                      #
+#     ------------------------ODOO WAVES----------------------         #
+#     --------------odoowaves.solution@gmail.com--------------         #
+#                                                                      #
+########################################################################
+{
+    "name": "Odoo Python Pip Installer",
+    "summary": """Install The Library With In The Odoo Environment in easily, directly and better way""",
+    "category": "Extra Tools",
+    "version": "18.0.1.0.0",
+    "sequence": 2,
+    "author": "Odoo Waves",
+    "license": "LGPL-3",
+    "website": "",
+    "description": """Python Library Installer""",
+    "data": [
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+        'wizard/pip_command_view.xml',
+        'wizard/message_wizard_view.xml'
+    ],
+    "images": ['static/description/banner.gif'],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "pre_init_hook":"pre_init_check",
+}
